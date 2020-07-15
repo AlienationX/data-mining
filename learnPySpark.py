8c6ab83eadfe158550a695653fa41c64b51b18d1
AlienationX <le7yi_ss@hotmail.com>
2020-07-15 17:15:02 +0800

    Update learnPySpark.py

--- learnPySpark.py
+++ learnPySpark.py
@@ -1,6 +1,18 @@
 # coding:utf-8
 # python3
 
+"""
+spark2-submit \
+--master yarn \
+--deploy-mode client \
+--executor-memory 2G \
+--num-executors 10 \
+--executor-cores 4 \
+--conf spark.pyspark.python=/home/work/app/python3/bin/python3 \
+/home/work/remote/code/script/tmp/learnPySpark.py
+"""
+
+import os
 import socket
 
 from pyspark.sql import SparkSession
@@ -10,12 +22,12 @@ from pyspark import SparkConf, SparkContext
 def environ_init():
     hostname = socket.gethostname()
     if "hadoop" not in hostname:
-        import os
-
         # os.environ["JAVA_HOME"] = "C:/Program Files/Java/jdk1.8.0_221"
         os.environ["HADOOP_HOME"] = "E:/Appilaction/hadoop-common-2.6.0-bin"
         os.environ["SPARK_HOME"] = "E:/Appilaction/spark-2.4.4-bin-hadoop2.6"
-        os.environ["PYSPARK_PYTHON"] = 'C:/Program Files/Python36/python.exe'
+
+        # 使用spark2-submit提交任务需要指定--conf spark.pyspark.python=/home/work/app/python3/bin/python3
+        os.environ["PYSPARK_PYTHON"] = "C:/Program Files/Python36/python.exe"
 
         # org.apache.hadoop.security.AccessControlException: Permission denied: user=Admin, access=READ_EXECUTE, inode="/user/hive/warehouse/medical.db/dim_date":hive:hive:drwxrwx--x
         os.environ["HADOOP_USER_NAME"] = "work"
@@ -43,7 +55,7 @@ def hive_sql():
     1.需要将hive-site.xml放到$SPARK_HOME/conf目录下，或者创建SparkSession时指定config
     config("hive.metastore.uris", "thrift://hadoop-prod00:9083")
     2.需要在C:\\Windows\\System32\\drivers\\etc\\hosts添加集群ip和机器名的映射
-    3.可选：需要将放到$SPARK_HOME/jars目录下
+    3.可选：需要将mysql-connector-java-5.1.48.jar放到$SPARK_HOME/jars目录下
     """
     spark = SparkSession.builder \
         .master("local") \
